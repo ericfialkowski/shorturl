e6283e38362cafef496babb9cdd1a7cfd66f0e1e
ericfialkowski <eric.fialkowski@gmail.com>
2020-02-24 10:10:33 -0700

    Added "bad words" checks

--- dao/abbreviator.go
+++ dao/abbreviator.go
@@ -2,20 +2,27 @@ package dao
 
 import (
 	"fmt"
+	"shorturl/environment"
 	"shorturl/rando"
 )
 
-func CreateAbbreviation(url string, dao ShortUrlDao) (string, error) {
-	u := ""
+func randString() string {
+	return rando.RandStrn(environment.GetEnvIntOrDefault("keysize", 5))
+}
 
-	abv := rando.RandStrn(5)
+func CreateAbbreviation(url string, dao ShortUrlDao) (string, error) {
+	abv := randString()
+	for BadWord(abv) {
+		abv = randString()
+	}
+
 	u, err := dao.GetUrl(abv)
-	for len(u) != 0 {
+	for len(u) != 0 && url != u {
 		u, err = dao.GetUrl(abv) // TODO: handle error
 		if err != nil {
 			return "", fmt.Errorf("error checking abbeviation %v", err)
 		}
-		abv = rando.RandStrn(5)
+		abv = randString()
 	}
 
 	return abv, nil
